25089d38647e15bc3cec5dbcd6ab5cbeaebb55e4
DarshanCode2005 <darshanthakare05@gmail.com>
2025-04-02 23:48:49 +0530

    Frontend

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const fetchQuote = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/random-quote");
+            const data = await response.json();
+            setQuote(`${data[0].q}`); // Extract quote
+            setAuthor(`${data[0].a}`);
+
+        } catch (error) {
+            console.error("Error fetching quote:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    return (
+        <div>
+            <h1>Random Quote Generator</h1>
+            <section className="quote-container">
+                  <h3>{quote || "Loading quote..."}</h3>
+                  <p>{`- ${author}`||"Loading Author..."}</p>
+            </section>
+            <button onClick={fetchQuote}>Get New Quote</button>
+        </div>
+    );
+}
+
+export default App;
